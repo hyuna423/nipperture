b96ab8849eca5c2bd2498040d2f2f63af657837a
Hyuna Kim <firstluv423@gmail.com>
2022-07-20 17:18:33 +0900

    header_first

--- js/menu_scroll.js
+++ js/menu_scroll.js
@@ -11,4 +11,17 @@ $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 });
 
 
+$(function(){
+    $(window).scroll(function(){
+    var navbar = $(this).scrollTop();
+    console.log(navbar);
+    var $header = $('header');
+    if(navbar > 860){
+        $header.addClass('activated');
+    }else{
+        $header.removeClass('activated');
+    }
+  })
+  })
+
 //메뉴 클릭시 자연스러운 스크롤 이동
